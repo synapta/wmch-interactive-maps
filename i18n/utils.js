57bc765f8ed8b832889354bac2de994bfc5fa11c
chirale <chirale@gmail.com>
2019-03-18 17:40:44 +0100

    Very useful comment

--- i18n/utils.js
+++ i18n/utils.js
@@ -16,9 +16,11 @@ function getShortlang (langcode) {
 function seekLang (req, fallbackLanguage) {
     let shortlang = null;
     let translationData = null;
+    // languages, desc by preferences, ready to be popped out
     let candidateLangs = req.acceptsLanguages().reverse();
     let found = null;
     let candidateLang = null;
+    // Looking for any available language in user browser, ordered by preference
     for (found  = false, candidateLang = candidateLangs.pop(); candidateLangs.length && !found; candidateLang = candidateLangs.pop()) {
         try {
             // navigator.language: if ?l=asd not defined, fallback to first accepted language
