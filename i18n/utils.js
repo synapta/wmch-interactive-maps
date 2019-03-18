cd89501b7c8d796bc781e87416370eef7f38775c
chirale <chirale@gmail.com>
2019-03-18 17:35:11 +0100

    Separate i18n functions, better theme

---
+++ i18n/utils.js
@@ -0,0 +1,41 @@
+const util = require('util');
+
+function loadTranslationFile (folder, shortlang) {
+    // return: a js object containing the JSON contents
+    return require(util.format('./%s/%s.json', folder, shortlang));
+}
+
+function getShortlang (langcode) {
+    let navigatorLanguage = langcode.toLowerCase();
+    if (navigatorLanguage.length > 2) {
+        navigatorLanguage = navigatorLanguage.split('-')[0];
+    }
+    return navigatorLanguage;
+}
+
+function seekLang (req, fallbackLanguage) {
+    let shortlang = null;
+    let translationData = null;
+    let candidateLangs = req.acceptsLanguages().reverse();
+    let found = null;
+    let candidateLang = null;
+    for (found  = false, candidateLang = candidateLangs.pop(); candidateLangs.length && !found; candidateLang = candidateLangs.pop()) {
+        try {
+            // navigator.language: if ?l=asd not defined, fallback to first accepted language
+            shortlang = getShortlang(req.query.l ? req.query.l : candidateLang);
+            translationData = loadTranslationFile('wizard', shortlang);
+            // found, do not continue
+            found = true;
+        }
+        catch (e) {
+            // none match use fallback language
+            shortlang = fallbackLanguage;
+            translationData = loadTranslationFile('wizard', fallbackLanguage);
+        }
+    }
+    return [shortlang, translationData];
+}
+
+exports.loadTranslationFile = loadTranslationFile;
+exports.getShortlang = getShortlang;
+exports.seekLang = seekLang;
--- urls.js
+++ urls.js
@@ -1,22 +1,13 @@
-var util = require('util');
-var fs = require('fs');
+const util = require('util');
+const fs = require('fs');
 //////////////////////////////////
-var express = require('express');
-var Mustache = require('mustache');
-var i18next = require('i18next');
-
-function loadTranslationFile (folder, shortlang) {
-    // return: a js object containing the JSON contents
-    return require(util.format('./i18n/%s/%s.json', folder, shortlang));
-}
-
-function getShortlang (langcode) {
-    let navigatorLanguage = langcode.toLowerCase();
-    if (navigatorLanguage.length > 2) {
-        navigatorLanguage = navigatorLanguage.split('-')[0];
-    }
-    return navigatorLanguage;
-}
+const express = require('express');
+const Mustache = require('mustache');
+const i18next = require('i18next');
+// Custom functions for internationalization
+const i18n_utils = require('./i18n/utils');
+// Settings
+const config = require('./config');
 
 // var request = require('request');
 module.exports = function(app, apicache, passport) {
@@ -25,8 +16,6 @@ module.exports = function(app, apicache, passport) {
     app.use('/',express.static('./public/frontend'));
 
     app.get('/wizard', async function (req, res) {
-        // navigator.language: if ?l=asd not defined, fallback to first accepted language
-        let shortlang = getShortlang(req.query.l ? req.query.l : req.acceptsLanguages()[0]);
         // [ 'it', 'it-IT', 'en-US', 'en' ]
         // console.log(req.acceptsLanguages()[0]);
         fs.readFile(util.format('%s/public/wizard/index.html', __dirname), function (err, fileData) {
@@ -35,23 +24,14 @@ module.exports = function(app, apicache, passport) {
             }
             // get template content, server-side
             let template = fileData.toString();
+            let [shortlang, translationData] = i18n_utils.seekLang(req, config.fallbackLanguage);
             let i18nOptions = {
               lng: shortlang,
               debug: true,
               resources: {}
             };
-            // load translation file from wizard/i18n/SHORTLANG.js
-            // default: English
-            let translationData = null;
-            try {
-                translationData = loadTranslationFile('wizard', shortlang);
-            }
-            catch (e) {
-                // pass
-                translationData = loadTranslationFile('wizard', 'en');
-            }
             i18nOptions.resources[shortlang] = {translation: translationData};
-            console.log(i18nOptions);
+            // console.log(i18nOptions);
             // load i18n
             i18next.init(i18nOptions, function(err, t) {
                 // i18n initialized and ready to go!
@@ -59,11 +39,9 @@ module.exports = function(app, apicache, passport) {
                 // variables to pass to Mustache to populate template
                 var view = {
                   shortlang: shortlang,
-                  title: "Joe",
                   i18n: function () {
                     return function (text, render) {
                         i18next.changeLanguage(shortlang);
-                        // return util.format("le tue frittelle facvano %s", text);
                         return i18next.t(text);
                     }
                   }
