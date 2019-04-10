b182994d51df1122472aa669b34d824e1d0fc0cc
chirale <chirale@gmail.com>
2019-04-10 13:48:45 +0200

    Screenshot bugfix /3

--- screenshot.js
+++ screenshot.js
@@ -37,7 +37,7 @@ const nodeurl = require('url');
           Object.assign(options, config.screenshotServer.options);
           // use path instead of full url to be protocol agnostic
           let urlob = nodeurl.parse(url);
-          options.screenshot = util.format(options.path, hasha(urlob.path));
+          options.path = util.format(options.path, hasha(urlob.path));
           // console.log('~~~~~~~~~~');
           // console.log(options.path);
           // console.log('~~~~~~~~~~');
