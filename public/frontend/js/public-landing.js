85e9f1ebe415f251a7437c6f6d14e5b25deed2d2
chirale <chirale@gmail.com>
2019-11-27 17:34:08 +0100

    Do not display 'Load others' if no more results. Use Open Sans regular, bold and semi-bold overriding even Semantic UI styles. New wikiblue color class. Display 20 elements on front page before showing 'Load others'.

--- public/frontend/js/public-landing.js
+++ public/frontend/js/public-landing.js
@@ -2,8 +2,8 @@
 
 $(function() {
     // var skip = 9;
-    var globLimit = 3;
-    var globOffset = 0;
+    var globLimit = 20;  // 2 elements x 10 rows
+    var globOffset = globLimit;
     // icon to display on pinned / favourite maps
     var starIcon = '<span class="ui left corner label yellow"><i class="star icon"></i></span>';
 
@@ -36,9 +36,12 @@ $(function() {
     }
 
     function loadMaps(limit, offset, callback) {
+        /**
+         ** Load offset + 1 maps (to check if there are others after the last)
+         **/
         $.ajax ({
             type:'GET',
-            url: "/api/all?offset=" + offset + "&limit=" + limit,
+            url: "/api/all?offset=" + offset + "&limit=" + parseInt(limit + 1),
             error: function(e) {
                 console.warn('Error retrieving data');
             },
@@ -47,44 +50,61 @@ $(function() {
                 // Add map preview
                 // add language when user selected it
                 var userDefinedLang = getUrlParameter('l');
+                var noMoreResults = !(mapResults.length > limit);
+                console.log("mapresults.length", mapResults.length);
+                console.log("limit", limit);
                 userDefinedLangQuery = userDefinedLang ? '?l=' + userDefinedLang : '';
                 $.each(mapResults, function( index, map ) {
-                    // map.href + userDefinedLangQuery
-                    var thisIsStarred = isStarred(map) ? starIcon : '';
-                    $('#infinite').append('<div class="column"><div class="ui raised segment">'
-                    + thisIsStarred
-                    + '<div class="square landingmapimage" style="background-image: url(' + map.screenshot + ');">'
-                    + '<a title="' + (map.title.length > 30 ? map.title : '') + '" href="' + map.href + userDefinedLangQuery + '" class="ui ' + colorLottery() + ' ribbon label">' + map.title.substring(0, 30) + (map.title.length > 30 ? '&hellip;' : '') + '</a>'
-                    + '</div>'
-                    + '</div>');
+                    if (index < limit) {
+                        // skip last element
+                        // map.href + userDefinedLangQuery
+                        var thisIsStarred = isStarred(map) ? starIcon : '';
+                        $('#infinite').append('<div class="column"><div class="ui raised segment">'
+                        + thisIsStarred
+                        + '<div class="square landingmapimage" style="background-image: url(' + map.screenshot + ');">'
+                        + '<a title="' + (map.title.length > 30 ? map.title : '') + '" href="' + map.href + userDefinedLangQuery + '" class="ui ' + colorLottery() + ' ribbon label">' + map.title.substring(0, 30) + (map.title.length > 30 ? '&hellip;' : '') + '</a>'
+                        + '</div>'
+                        + '</div>');
+                    }
                 });
                 // preview added, load
-                callback(mapResults);
+                callback(mapResults, noMoreResults);
             }
         });
     }
 
     // prepare to load other images
-    globOffset = 9;
     // loadMaps(0);
     // load first 9 elements, no offset
-    loadMaps(globOffset, 0, function (results) {
-        // pass
+    loadMaps(globOffset, 0, function (results, noMoreResults) {
+        console.log(noMoreResults);
+        if (!noMoreResults) {
+            $("#loadothers").removeClass("donotdisplay");
+        }
     });
-    $("#loadothers").on("click", function () {
-        var step = 3;
+    $("#loadothers").on("click", function (ev) {
+        // do not back to top
+        ev.preventDefault();
+        var step = 2;
         // show loading button
         $("#loadothers").toggleClass('loading');
-        loadMaps(step, globOffset, function (mapResults) {
+        loadMaps(step, globOffset, function (mapResults, noMoreResults) {
             // elements loaded on page
             // prepare offset for next request
             globOffset += step;
             // toggle loading
             $("#loadothers").toggleClass('loading');
-            if (mapResults.length == 0) {
+            if (noMoreResults) {
                 // no others
                 $("#loadothers").addClass('disabled');
                 $("#loadothers").text($('#loadothers').data("noresults"));
+                // hide
+                $("#loadothers").addClass("donotdisplay");
+            }
+            else {
+                $("#loadothers").removeClass("donotdisplay");
+                // Scroll to bottom
+                $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 800);
             }
         });
     });
