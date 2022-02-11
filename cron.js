6548d92806687c91d0998561ccd19c4febd0f60e
chirale <chirale@gmail.com>
2022-02-11 11:12:14 +0100

    Now map.history must be true to update history periodically.

--- cron.js
+++ cron.js
@@ -26,78 +26,82 @@ if (argv['nosentry']) {
 logger.debug('Before job instantiation');
 // @see https://github.com/kelektiv/node-cron/blob/master/examples/every_10_minutes.js
 const job = new CronJob(localconfig.cronTime, async function() {
-		const d = new Date();
-		logger.debug('Every 10 Minutes:', d);
-		//////////////////////////////////////////
+	const d = new Date();
+	logger.debug('Every 10 Minutes:', d);
+	//////////////////////////////////////////
 
-		async function timeshot(maps) {
-			/**
-			 *  Add a new timeshot to History table
-			 *  popping one Map record after another until it's consumed.
-			 *
-			 *  @param {object} maps: array of sequelize result Map objects
-			 **/
-			  function timeshotDo() {
-						timeshot(maps);
-				}
+	async function timeshot(maps) {
+		/**
+		 *  Add a new timeshot to History table
+		 *  popping one Map record after another until it's consumed.
+		 *
+		 *  @param {object} maps: array of sequelize result Map objects
+		 **/
+			function timeshotDo() {
+					timeshot(maps);
+			}
 
-				let record = maps.pop();
+			let record = maps.pop();
 
-	      if (record) {
-						History.findAll({
-							where: {
-								mapId: record.id
-							},
-							include: [{
-									model: Map,
-									where: {
-										published: true
-									}
+			if (record) {
+					// get last History record
+					History.findAll({
+						where: {
+							mapId: record.id
+						},
+						include: [{
+								model: Map,
+								where: {
+									history: true,
+									published: true
 								}
-							],
-							order: [
-								// inverse order
-								['createdAt', 'DESC']
-							],
-							limit: 1
-						}).then(async hists => {
-								let hist = hists.pop();
-								let ob = dbutils.mapargsParse(record);
-								// prepare JSON to be written on database
-								let currentObj = await data.getJSONfromQuery(ob.query, "cron.js");
-                // get from database the last saved record as string, hydrate it to object and remove our metadata postProcess
-                let beforeObj = (typeof hist == 'undefined') ? false : diff.removePostProcess(JSON.parse(hist.json));
-                // isDifferent if 1) is a new record or 2) is identical to previous record (using node.js assert)
-                let isDifferent = (typeof hist == 'undefined') ? true : diff.isStrictDifferent (beforeObj, currentObj);
-                let jsonValue = '';
-                if (beforeObj) {
-                    // alter currentObj adding .postProcess on each feature if changed
-                    await diff.postProcess(beforeObj, currentObj);
-                }
-                // create a new History record
-								await History.create({
-									mapId: record.id,
-                  // do not add postProcess if fist element
-									json: JSON.stringify(currentObj),
-									diff: isDifferent,
-									error: currentObj.error
-								});
-								// regenerate another after msCronWaitWikidata milliseconds
-								setTimeout(timeshotDo, localconfig.msCronWaitWikidata);
+							}
+						],
+						order: [
+							// inverse order
+							['createdAt', 'DESC']
+						],
+						limit: 1
+					}).then(async hists => {
+						// looking for differences
+						let hist = hists.pop();
+						let ob = dbutils.mapargsParse(record);
+						// prepare JSON to be written on database
+						let currentObj = await data.getJSONfromQuery(ob.query, "cron.js");
+						// get from database the last saved record as string, hydrate it to object and remove our metadata postProcess
+						let beforeObj = (typeof hist == 'undefined') ? false : diff.removePostProcess(JSON.parse(hist.json));
+						// isDifferent if 1) is a new record or 2) is identical to previous record (using node.js assert)
+						let isDifferent = (typeof hist == 'undefined') ? true : diff.isStrictDifferent (beforeObj, currentObj);
+						let jsonValue = '';
+						if (beforeObj) {
+							// alter currentObj adding .postProcess on each feature if changed
+							await diff.postProcess(beforeObj, currentObj);
+						}
+						// create a new History record
+						await History.create({
+							mapId: record.id,
+							// do not add postProcess if first element
+							json: JSON.stringify(currentObj),
+							diff: isDifferent,
+							error: currentObj.error
 						});
-				}
-        else {
-            logger.info("*** cron timeshot end ***");
-        }
-		}
+						// regenerate another after msCronWaitWikidata milliseconds
+						setTimeout(timeshotDo, localconfig.msCronWaitWikidata);
+					});
+			}
+			else {
+				logger.info("*** cron timeshot end ***");
+			}
+	}
 
 
-    // init ////////////////////////////////////////////////////////////////////
+    	// init ////////////////////////////////////////////////////////////////////
 		// save history only of published maps
 		Map.findAll({
 			where: {
+				history: true,
 				published: true,
-        // id: 10  // DEBUG, only one map
+        		// id: 10  // DEBUG, only one map
 			},
 			order: [
 				// prepare results array for pop(), so order of execution will be from id 1 to N
