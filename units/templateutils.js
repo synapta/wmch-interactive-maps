067c612916ae3051d6bae2e509d6b33ce5dad8d8
chirale <chirale@gmail.com>
2022-03-02 16:43:25 +0100

    Add Not Found page and not found utils

--- units/templateutils.js
+++ units/templateutils.js
@@ -1,14 +1,49 @@
 "use strict";
 
-const { promises: fsa } = require("fs");
+const { promises: fs } = require("fs");
+const Mustache = require('mustache');
+// Global settings
+const config = require('../config');
+const i18n_utils = require('../i18n/utils');
+const localconfig = require('../localconfig');
 
-exports.readMustachePartials = async (key) => {
-    let fileData = await fsa.readFile(`${key}`);
+const readMustachePartials = async (key) => {
+    let fileData = await fs.readFile(`${key}`);
     // get template content, server-side
     let template = fileData.toString();
     return template;
 };
+exports.readMustachePartials = readMustachePartials;
 
-exports.notFound = async (context, message) => {
-    return `<h2>${message ? message : "Not Found"}</h2>`;
+/**
+ * 
+ * @param {Object} resource from Express
+ * @param {String} message not found message
+ * @returns 
+ */
+exports.notFound = async (request, text) => {
+    const message = text ? text : "Not Found"
+    const comment = "Content you're looking for doesn't exist"
+    console.log(__dirname)
+    const templatePath = `${__dirname}/../public/frontend/notfound.mustache`
+    let output = `<h2>${text}</h2>`
+    try {
+        let template = await fs.readFile(templatePath)
+        let [shortlang, translationData] = i18n_utils.seekLang(request, config.fallbackLanguage, 'frontend')
+        output = Mustache.render(template.toString(), { 
+            message: message, 
+            comment: comment,
+            langname: i18n_utils.getLangName(config.languages, shortlang),
+            // common
+            baseurl: localconfig.url + "/",
+            languages: config.languages,
+            author: config.map.author 
+        })
+    }
+    catch (e) {
+        // pass to error handling
+        console.error("Cannot read template file")
+        console.log(e)
+    }
+    return output
 }
