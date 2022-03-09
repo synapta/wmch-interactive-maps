e41ab501c692ef3bb74dfc0c8123b92638ca9edd
chirale <chirale@gmail.com>
2022-03-09 11:23:12 +0100

    Add mail unit to s end mail through Ask Your Map

---
+++ units/mail.js
@@ -0,0 +1,46 @@
+"use strict";
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+const config = require('../config');
+const localconfig = require('../localconfig');
+
+let mailTransporter;
+if (typeof localconfig.mailgun !== "undefined") {
+    mailTransporter = nodemailer.createTransport(mg(localconfig.mailgun));
+}
+
+const sendMail = function (data) {
+    const message = {
+        from: `${data.contactName} <${localconfig.mailgun.mailFrom}>`,
+        'h:Reply-To': `${data.contactName} <${data.contactMail}>`,
+        to: localconfig.mailgun.mailTo,
+        subject: `[${config.appname}] ${data.contactName} proposed a new map`,
+        text: `Message is: 
+
+${data.comment}
+~~~
+
+Query SPARQL is:
+
+${data.sparql}
+
+~~~
+
+-- 
+${config.appname} / Ask Your Map
+`
+    };
+    return mailTransporter.sendMail(message);
+}
+exports.sendMail = sendMail;
+
+const test = () => {
+    const data = {
+        contactMail: "emanuele@synapta.it",
+        contactName: "emanuele",
+        comment: "Ciao mondo",
+        sparql: "Query qui"
+    }
+    sendMail(data)
+}
+test();
