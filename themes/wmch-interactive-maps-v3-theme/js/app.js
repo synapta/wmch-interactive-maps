bdc3fb7838faa6de37b4943b810a238df7c2bb46
chirale <chirale@gmail.com>
2022-03-22 11:15:52 +0100

    Scroll effects, prepare Magellan for future use

--- themes/wmch-interactive-maps-v3-theme/js/app.js
+++ themes/wmch-interactive-maps-v3-theme/js/app.js
@@ -1,4 +1,24 @@
 $(document).foundation();
+
+$.fn.visible = function() {
+    var top = $(this).offset().top;
+    var bottom = top + $(this).outerHeight();
+    var vpTop = $(window).scrollTop();
+    var vpBottom = vpTop + $(window).height();
+    // See for example:
+    // https://medium.com/talk-like/detecting-if-an-element-is-in-the-viewport-jquery-a6a4405a3ea2
+    return bottom > vpTop && top < vpBottom;
+};
+
+function showCategories () {
+    $( ".category" ).each(function () {
+        console.log(this)
+        if ($(this).visible()) {
+            $(this).parents(".category-section").first().animate({"padding-top": "0"});
+            $(this).animate({"margin-bottom": "0.5rem"});
+        }
+    });
+}
 // Front page
 // lazy loads elements with default selector as '.lozad'
 const observer = lozad();
@@ -8,7 +28,5 @@ $(".seeall").click(function () {
     $(this).parents(".category-section").first().next().find(".map.hide").each(function () {$(this).removeClass("hide")})
     $(this).fadeOut();
 });
-$( ".category-section" ).click(function() {
-    $( this ).css("padding-top", "0");
-    $( this ).find(".category").first().css("margin-bottom", "0");
-});
+
+setInterval(showCategories, 300)
